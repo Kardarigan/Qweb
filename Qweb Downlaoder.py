332765ed9a7e997c3a5a44e517da7330f39d2fe0
DockDock Stay <113527210+Kardarigan@users.noreply.github.com>
2023-09-05 13:15:42 -0400

    Now its can extract albums and full albums links

--- Qweb Downlaoder.py
+++ Qweb Downlaoder.py
@@ -1,16 +1,19 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
 finalDestination = input('Enter Destination Index : ')
 finalDestination = int(finalDestination)
 finalDestination += 1
-
+nowTime = datetime.datetime.now()
+nowTime = nowTime.strftime('%Y%m%d%H%M')
+nowFileName = ''.join(filter(str.isdigit, nowTime))
 with open('allLinks.txt', 'w') as backup:
     pass
 
 for pageIntex in range(1, finalDestination):
     url = f'https://musicguitars.ir/page/{pageIntex}/'
-    artistNames = ['ابی', 'اندی', 'شهره', 'گوگوش', 'مارتیک', 'تتلو']
+    artistNames = ['ابی', 'اندی', 'شهره', 'گوگوش', 'مارتیک']
 
     try:
         response = requests.get(url)
@@ -18,7 +21,7 @@ for pageIntex in range(1, finalDestination):
             mainContent = BeautifulSoup(response.text, 'html.parser')
             posterts = mainContent.find_all('article', class_='gmmpost')
             for post in posterts:
-                h2Tag = post.find("h2", class_="gmmah")
+                h2Tag = post.find('h2', class_='gmmah')
                 if h2Tag:
                     aTag = h2Tag.find('a', href=True)
                     aTagLink = aTag['href']
@@ -27,10 +30,10 @@ for pageIntex in range(1, finalDestination):
                         print(f'Founded : {aText} ---- Page : {pageIntex}')
                         response_article = requests.get(aTagLink)
                         if response_article.status_code == 200:
-                            mainContent_article = BeautifulSoup(
-                                response_article.text, 'html.parser')
-                            dlContainer = mainContent_article.find(
-                                "div", class_="gmdllink gmfx")
+                            mainContent_article = BeautifulSoup(response_article.text, 'html.parser')
+                            dlContainer = mainContent_article.find('div', class_='gmdllink gmfx')
+                            dlContainerAlbum = mainContent_article.find('div', class_='gmmpostc')
+
                             if dlContainer:
                                 dlLink = dlContainer.find_all(
                                     'a', class_='dl', href=True)
@@ -38,7 +41,16 @@ for pageIntex in range(1, finalDestination):
                                     dlLinkHref = dl['href']
                                     if dlLinkHref.endswith('[320].mp3'):
                                         print(dlLinkHref)
-                                        with open('allLinks.txt', 'a') as backup:
+                                        with open(f'Qweb-{nowFileName}.txt', 'a') as backup:
+                                            backup.write(f'{dlLinkHref}\n')
+                            elif dlContainerAlbum:
+                                dlLink = dlContainerAlbum.find_all(
+                                    'a', class_='gmsdl', href=True)
+                                for dl in dlLink:
+                                    dlLinkHref = dl['href']
+                                    if dlLinkHref.endswith('[320].mp3'):
+                                        print(dlLinkHref)
+                                        with open(f'Qweb-{nowFileName}.txt', 'a') as backup:
                                             backup.write(f'{dlLinkHref}\n')
                             else:
                                 print('Download container not found.')
