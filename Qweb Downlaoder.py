b586071ca85bc8c32914784d25fbed3333e819b5
DockDock Stay <113527210+Kardarigan@users.noreply.github.com>
2023-09-05 09:22:17 -0400

    Add Page nvigator

--- Qweb Downlaoder.py
+++ Qweb Downlaoder.py
@@ -1,55 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://musicguitars.ir/page/2/'
-artistNames = ['ابی', 'اندی', 'شهره', 'گوگوش', 'مارتیک']
+finalDestination = input('Enter Destination Index : ')
+finalDestination = int(finalDestination)
+finalDestination += 1
 
-try:
-    response = requests.get(url)
+with open('allLinks.txt', 'w') as backup:
+    pass
 
-    if response.status_code == 200:
-        mainContent = BeautifulSoup(response.text, 'html.parser')
-        posterts = mainContent.find_all('article', class_='gmmpost')
+for pageIntex in range(1, finalDestination):
+    url = f'https://musicguitars.ir/page/{pageIntex}/'
+    artistNames = ['ابی', 'اندی', 'شهره', 'گوگوش', 'مارتیک', 'تتلو']
 
-        for post in posterts:
-            h2Tag = post.find("h2", class_="gmmah")
-            if h2Tag:
-                aTag = h2Tag.find('a', href=True)
-                if aTag:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            mainContent = BeautifulSoup(response.text, 'html.parser')
+            posterts = mainContent.find_all('article', class_='gmmpost')
+            for post in posterts:
+                h2Tag = post.find("h2", class_="gmmah")
+                if h2Tag:
+                    aTag = h2Tag.find('a', href=True)
                     aTagLink = aTag['href']
                     aText = aTag.text.strip()
-
                     if any(name in aText for name in artistNames):
-                        print(f'Founded : {aText}')
-
+                        print(f'Founded : {aText} ---- Page : {pageIntex}')
                         response_article = requests.get(aTagLink)
-
                         if response_article.status_code == 200:
                             mainContent_article = BeautifulSoup(
                                 response_article.text, 'html.parser')
                             dlContainer = mainContent_article.find(
                                 "div", class_="gmdllink gmfx")
-
                             if dlContainer:
-                                dlLink = dlContainer.find_all('a', class_='dl', href=True)
+                                dlLink = dlContainer.find_all(
+                                    'a', class_='dl', href=True)
                                 for dl in dlLink:
                                     dlLinkHref = dl['href']
                                     if dlLinkHref.endswith('[320].mp3'):
                                         print(dlLinkHref)
-
-
-                                
+                                        with open('allLinks.txt', 'a') as backup:
+                                            backup.write(f'{dlLinkHref}\n')
                             else:
                                 print('Download container not found.')
                         else:
                             print(
                                 f'Error opening article link: {response_article.status_code}')
-            else:
-                print('Nothing Found :)')
-                backup = open('q.txt', 'w')
-                backup.write(str(mainContent))
-    else:
-        print(f'Error : {response.status_code}')
-
-except requests.exceptions.RequestException as e:
-    print(f'Could not Connect: {str(e)}')
+                else:
+                    print('Nothing Found :)')
+        else:
+            print(f'Error : {response.status_code}')
+    except requests.exceptions.RequestException as e:
+        print(f'Could not Connect: {str(e)}')
